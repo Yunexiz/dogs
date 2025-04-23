eaf941337119de0c52d90cdfe9471e2df187da9e
Yosuke Nishiyama <141266522+Yunexiz@users.noreply.github.com>
2025-04-23 11:33:37 +0100

    revamp

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-const apiUrl = 'https://api.thedogapi.com/v1/images/search?limit=10';
-const buttonContainer = document.getElementById('button-container');
+const apiUrl = "https://api.thedogapi.com/v1/images/search?limit=10";
+const buttonContainer = document.getElementById("button-container");
 
 fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    data.forEach((cat, index) => {
-      const button = document.createElement('button');
-      button.textContent = `Dog ${index + 1}`;
-      button.addEventListener('click', () => {
-        window.location.href = `/search?url=${cat.url}`;
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((dog, index) => {
+      const button = document.createElement("button");
+      const prefixes = [
+        "Dog",
+        "Puppy",
+        "Doge",
+        "Cute",
+        "Cutie",
+        "Paw",
+        "Woof",
+        "Bark",
+        "Slobber",
+      ];
+      const randomPrefix =
+        prefixes[Math.floor(Math.random() * prefixes.length)];
+      button.textContent = `${randomPrefix} ${index + 1}`;
+      button.addEventListener("click", () => {
+        window.location.href = `/display?url=${dog.url}`;
       });
       buttonContainer.appendChild(button);
     });
   })
-  .catch(error => {
-    console.error('Error fetching dog images:', error);
+  .catch((error) => {
+    console.error("Error fetching dog images:", error);
   });
-
-
