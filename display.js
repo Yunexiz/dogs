c58a734f6caef78fe6a81265a0645a234484131a
Yosuke Nishiyama <141266522+Yunexiz@users.noreply.github.com>
2025-04-23 11:37:55 +0100

    embed js

--- display.js
+++
@@ -1,5 +0,0 @@
-const imageElement = document.getElementById('dog-image');
-
-const urlParams = new URLSearchParams(window.location.search);
-const imageUrl = urlParams.get('url');
-imageElement.src = imageUrl;
